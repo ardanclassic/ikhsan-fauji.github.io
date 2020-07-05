5555608f6e2c514b3e0bedb3fb28b2da15fb2aa4
Ikhsan Fauji <ikhsan.fauzi@btpn.com>
2020-07-05 16:19:43 +0700

    v.140

--- service-worker.js
+++ service-worker.js
@@ -1,65 +1,60 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js');
 
 workbox.precaching.precacheAndRoute([
-  { url: './', revision: '111' },
-  { url: './index.html', revision: '111' },
-  { url: './home.html', revision: '111' },
-  { url: './match.html', revision: '111' },
-  { url: './clubs.html', revision: '111' },
-  { url: './club-detail.html', revision: '111' },
-  { url: './favorite-clubs.html', revision: '111' },
-  { url: './saved-match.html', revision: '111' },
-  { url: './main.js', revision: '111' },
-  { url: './manifest.json', revision: '111' }
+  { url: './', revision: '140' },
+  { url: './index.html', revision: '140' },
+  { url: './home.html', revision: '140' },
+  { url: './match.html', revision: '140' },
+  { url: './clubs.html', revision: '140' },
+  { url: './club-detail.html', revision: '140' },
+  { url: './favorite-clubs.html', revision: '140' },
+  { url: './saved-match.html', revision: '140' },
+  { url: './main.js', revision: '140' },
+  { url: './manifest.json', revision: '140' },
+  { url: './images/logo.png', revision: '140' },
+  { url: './images/stadion.jpg', revision: '140' }
 ]);
 
 workbox.routing.registerRoute(
-  /\.(?:html|js|json)$/,
-  workbox.strategies.cacheFirst()
+  new RegExp('.svg'),
+  new workbox.strategies.CacheFirst({
+      cacheName: 'logo'
+  })
 );
 
 workbox.routing.registerRoute(
   new RegExp('/images/'),
-  workbox.strategies.cacheFirst({
-      cacheName: 'images',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 30 * 24 * 60 * 60
-        })
-      ]
+  new workbox.strategies.CacheFirst({
+      cacheName: 'images'
   })
 );
 
 workbox.routing.registerRoute(
-  new RegExp('/icons/'),
-  workbox.strategies.cacheFirst({
-      cacheName: 'icons',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxAgeSeconds: 30 * 24 * 60 * 60
-        })
-      ]
+  new RegExp('/icons/android/'),
+  new workbox.strategies.CacheFirst({
+      cacheName: 'android-icons'
   })
 );
 
 workbox.routing.registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  workbox.strategies.cacheFirst({
+  new RegExp('/icons/AppIcon.appiconset/'),
+  new workbox.strategies.CacheFirst({
+      cacheName: 'apple-icons'
+  })
+);
+
+workbox.routing.registerRoute(
+  ({url}) => url.origin === 'https://fonts.googleapis.com' ||
+             url.origin === 'https://fonts.gstatic.com',
+  new workbox.strategies.CacheFirst({
     cacheName: 'google-fonts-stylesheets'
   })
 );
 
 workbox.routing.registerRoute(
-  /^https:\/\/api\.football-data\.org/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'football-data'
-  })
-);
-
-workbox.routing.registerRoute(
-  /^https:\/\/momentjs\.com/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'moment'
+  ({url}) => url.origin === 'https://api.football-data.org',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'football-data',
   })
 );
 
